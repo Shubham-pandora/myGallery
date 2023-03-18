10d7cccbfeeab896232056b0d9cc66aaea795c47
Shubham-pandora <94621932+Shubham-pandora@users.noreply.github.com>
2023-03-18 13:12:17 +0530

    Update settings.py

--- myGallery/settings.py
+++ myGallery/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-1r=l1%n#j@zfh4z4xksgo0r(+p7rdik9t_j@3@7p_*@t+c#+8v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
